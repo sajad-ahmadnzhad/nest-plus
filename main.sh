da0f1a813b0ca0f413584f4ea42494c02e70e0d6
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-07 17:09:24 +0330

    feat: remove line in package.json

--- main.sh
+++ main.sh
@@ -15,11 +15,12 @@ if [ $? -eq 0 ]; then
     mv app.module.ts modules
     cd "../"
 
-    yesOrNo "prettierrc and a eslintrc.js be removed?"
+    yesOrNo "prettierrc and eslintrc.js be removed?"
 
     if [ $INPUT = "y" ]; then 
     rm .eslintrc.js
     rm .prettierrc
+    sed -i "10d;15d;37,41d;43d" package.json
     fi
 
     yesOrNo "Test files are deleted?"
@@ -27,6 +28,15 @@ if [ $? -eq 0 ]; then
     if [ $INPUT = "y" -a -d "test" ]; then
         rm -rf 'test'
         rm src/app.controller.spec.ts
+
+        if [ -f ".eslintrc.js" -a -f ".prettierrc" ]; then
+            sed -i "16,20d;32d;34d;36d;42d;45,46d;52,68d" package.json
+            sed -i "15s/,$//;40s/,$//" package.json
+        else 
+            sed -i "14,18d;30d;32d;34,35d;37,38d;44,60d" package.json
+            sed -i "13s/,$//;32s/,$//" package.json
+        fi
+
     fi
 
       {
