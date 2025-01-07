6af5a176de804f0bedb573a814afb09ebf907ef4
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-07 19:41:17 +0330

    feat: add .gitignore & add generate typeorm method

--- main.sh
+++ main.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
-# read -t 10 -p "Enter project name: " "PROJECT_NAME"
+read -t 10 -p "Enter project name: " "PROJECT_NAME"
 
-# npx nest new $PROJECT_NAME --skip-install
+npx nest new $PROJECT_NAME --skip-install
 
-# if [ $? -eq 0 ]; then
-#     source utils.sh
-#     cd "$PROJECT_NAME/src"
-#     mkdir "common"
-#     mkdir "configs"
-#     mkdir "modules"
-#     rm app.controller.ts
-#     rm app.service.ts
-#     mv app.module.ts modules
-#     cd "../"
+if [ $? -eq 0 ]; then
+    source utils.sh
+    cd "$PROJECT_NAME/src"
+    mkdir "common"
+    mkdir "configs"
+    mkdir "modules"
+    mkdir "modules/app"
+    rm app.controller.ts
+    rm app.service.ts
+    mv app.module.ts modules/app
+    cd "../"
+    touch .env
+    touch .env.example
 
-#     yesOrNo "prettierrc and eslintrc.js be removed?"
+    yesOrNo "prettierrc and eslintrc.js be removed?"
 
-#     if [ $INPUT = "y" ]; then 
-#     rm .eslintrc.js
-#     rm .prettierrc
-#     sed -i "10d;15d;37,41d;43d" package.json
-#     fi
+    if [ $INPUT = "y" ]; then 
+    rm .eslintrc.js
+    rm .prettierrc
+    sed -i "10d;15d;37,41d;43d" package.json
+    fi
 
-#     yesOrNo "Test files are deleted?"
+    yesOrNo "Test files are deleted?"
 
-#     if [ $INPUT = "y" -a -d "test" ]; then
-#         rm -rf 'test'
-#         rm src/app.controller.spec.ts
+    if [ $INPUT = "y" -a -d "test" ]; then
+        rm -rf 'test'
+        rm src/app.controller.spec.ts
 
-#         if [ -f ".eslintrc.js" -a -f ".prettierrc" ]; then
-#             sed -i "16,20d;32d;34d;36d;42d;45,46d;52,68d" package.json
-#             sed -i "15s/,$//;40s/,$//" package.json
-#         else 
-#             sed -i "14,18d;30d;32d;34,35d;37,38d;44,60d" package.json
-#             sed -i "13s/,$//;32s/,$//" package.json
-#         fi
+        if [ -f ".eslintrc.js" -a -f ".prettierrc" ]; then
+            sed -i "16,20d;32d;34d;36d;42d;45,46d;52,68d" package.json
+            sed -i "15s/,$//;40s/,$//" package.json
+        else 
+            sed -i "14,18d;30d;32d;34,35d;37,38d;44,60d" package.json
+            sed -i "13s/,$//;32s/,$//" package.json
+        fi
 
-#     fi
-#     changeAppModule
-
-# fi
-
-CHOICE=$(printf "TypeORM\nPrisma\nMicroORM\nMongoose\nNo Database" | fzf --prompt="Select ORM or ODM: ")
+    fi
+    changeAppModule
+    INSTALL_PACKAGES=()
+    CHOICE=$(printf "TypeORM\nPrisma\nMicroORM\nMongoose\nNo Database" | fzf --prompt="Select ORM or ODM: ")
 
     if [ -n "$CHOICE" ]; then
         case $CHOICE in 
             "TypeORM")
-             echo "This is typeorm"
+            cd src
+            generateTypeorm
              ;;
              "MicroORM")
              echo "This is microorm"
@@ -66,3 +68,6 @@ CHOICE=$(printf "TypeORM\nPrisma\nMicroORM\nMongoose\nNo Database" | fzf --promp
         ceho "No item was selected"
         exit 1
     fi
+
+fi
+
--- utils.sh
+++ utils.sh
@@ -22,7 +22,59 @@ function changeAppModule() {
         echo "  providers: []"
         echo "})"
         echo "export class AppModule {}"
-    } > src/modules/app.module.ts
+    } > src/modules/app/app.module.ts
     
+    return 0
+}
+
+function generateTypeorm() {
+
+    cd configs
+    touch typeorm.config.ts
+
+cat << EOF > typeorm.config.ts
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeormConfig = (): TypeOrmModuleOptions => {
+  const {
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_SYNCHRONIZE,
+  } = process.env;
+
+  return {
+    type: "postgres",
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    synchronize: !!Number(DB_SYNCHRONIZE),
+    entities: [],
+    autoLoadEntities: false,
+  };
+};
+EOF
+
+cd "../modules"
+
+cat << EOF > app/app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeormConfig } from "../../configs/typeorm.config";
+
+@Module({
+  imports: [ TypeOrmModule.forRoot(typeormConfig()) ],
+  controllers: [],
+  providers: []
+})
+export class AppModule {}
+EOF
+
+INSTALL_PACKAGES+=("typeorm" "@nestjs/typeorm")
+
     return 0
 }
