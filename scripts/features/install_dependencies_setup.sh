44328b2be2f88e6518900b05187ff565225497dd
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-14 17:10:03 +0330

    feat: remove installation aditional packages method from install dependecnies

--- scripts/features/install_dependencies_setup.sh
+++ scripts/features/install_dependencies_setup.sh
@@ -4,13 +4,9 @@ source scripts/config/config.sh
 function installPackages() {
   source "$INSTALL_PACKAGES_FILE"
   cd $PROJECT_NAME
-  echo -e "${BLUE}Please Wait for installing ${INSTALL_PACKAGES[@]} packages.....${RESET}"
+  echo -e "${BLUE}Please Wait for installing npm packages.....${RESET}"
   echo -e "${YELLOW}If there is a problem installing the packages, enter the following command: $2 ${INSTALL_PACKAGES[@]}${RESET}"
   command $2 "${INSTALL_PACKAGES[@]}"
-
-  echo -e "${BLUE}Wait for installing additional packages.....${RESET}"
-  echo -e "${YELLOW}If there is a problem installing the packages, enter the following command: $1${RESET}"
-  command $1
 
   cleanupTmpFile
   echo -e "${GREEN}Thanks for using nest-plus${RESET}"
